54857992e116ddafa9cd2a63c2a8bd179967ecd2
Artak1999 <dashyan.1999@mail.ru>
2022-08-20 14:09:07 +0500

    Isn't easy.

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -1,7 +1,6 @@
 package com.epam.mjc.io;
 
 import java.io.*;
-import java.util.Arrays;
 
 public class FileReader {
 
@@ -15,27 +14,21 @@ public class FileReader {
                 result += (char) i;
             }
             System.out.println(result);
+            for (int j = 0; j < result.length(); j++) {
+                if(result.contains("Name: "))
+                    result = result.replace("Name: ","");
+                if(result.contains("Age: "))
+                    result = result.replace("Age: ","");
+                if(result.contains("Email: "))
+                    result = result.replace("Email: ","");
+                if(result.contains("Phone: "))
+                    result = result.replace("Phone: ","");
+            }
             String[] lines = result.split("\\R");
             String name = lines[0];
             String age = lines[1];
             String email = lines[2];
             String phone = lines[3];
-            for (int j = 0; j < name.length(); j++) {
-                if(name.contains("Name: "))
-                    name = name.replace("Name: ","");
-            }
-            for (int j = 0; j < age.length(); j++) {
-                if(age.contains("Age: "))
-                    age = age.replace("Age: ","");
-            }
-            for (int j = 0; j < email.length(); j++) {
-                if(email.contains("Email: "))
-                    email = email.replace("Email: ","");
-            }
-            for (int j = 0; j < phone.length(); j++) {
-                if(phone.contains("Phone: "))
-                    phone = phone.replace("Phone: ","");
-            }
             profile.setName(name);
             profile.setAge(Integer.parseInt(age));
             profile.setEmail(email);
